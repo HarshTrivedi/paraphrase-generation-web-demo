1b4a0ea4f36699e5da012cffb6478c0398bcfc75
harshtrivedi <harshjtrivedi94@gmail.com>
2016-07-11 12:36:58 +0530

    add graphviz build on heroku

--- web_app.py
+++ web_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<b>UnderConstruction ... </b>"
+    return "<b>Under Construction ... </b>"
 
 if __name__ == "__main__":
     app.run()
