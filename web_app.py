ed2d9ea819bf03b87ab46bdb8f9ae74cb53cbd4a
Harsh Trivedi <harshjtrivedi94@gmail.com>
2016-07-22 18:25:30 +0530

    Try explicit error log.

--- web_app.py
+++ web_app.py
@@ -6,16 +6,18 @@ from awesome_print import ap
 
 @app.route('/')
 def home():
-	equivalent_lines = request.args.get("equivalent_lines")
-	if equivalent_lines is not None:
-		equivalent_lines = equivalent_lines.strip().split("\n")
-		equivalent_lines = map( lambda line: line.strip(),  equivalent_lines)
-		fsm_code = get_fsm_code( equivalent_lines)
-		fsm_svg = get_svg(fsm_code)
-	else:
-		fsm_svg = ""
-	return render_template('index.html', svg = fsm_svg, equivalent_lines = equivalent_lines)
-
+	try:
+		equivalent_lines = request.args.get("equivalent_lines")
+		if equivalent_lines is not None:
+			equivalent_lines = equivalent_lines.strip().split("\n")
+			equivalent_lines = map( lambda line: line.strip(),  equivalent_lines)
+			fsm_code = get_fsm_code( equivalent_lines)
+			fsm_svg = get_svg(fsm_code)
+		else:
+			fsm_svg = ""
+		return render_template('index.html', svg = fsm_svg, equivalent_lines = equivalent_lines)
+	except:
+		traceback.print_exc(file=sys.stdout)
 
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
