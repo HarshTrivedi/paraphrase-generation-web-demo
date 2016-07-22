ef6d0b9e4b42aaa2bf639b3ebc02f8c3b6988ff4
Harsh Trivedi <harshjtrivedi94@gmail.com>
2016-07-22 18:48:13 +0530

    update webapp.py to show errors in production. Attempt XXX

--- web_app.py
+++ web_app.py
@@ -4,20 +4,21 @@ app = Flask(__name__)
 from main_lib import *
 from awesome_print import ap
 
+import logging, sys
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
 @app.route('/')
 def home():
-	try:
-		equivalent_lines = request.args.get("equivalent_lines")
-		if equivalent_lines is not None:
-			equivalent_lines = equivalent_lines.strip().split("\n")
-			equivalent_lines = map( lambda line: line.strip(),  equivalent_lines)
-			fsm_code = get_fsm_code( equivalent_lines)
-			fsm_svg = get_svg(fsm_code)
-		else:
-			fsm_svg = ""
-		return render_template('index.html', svg = fsm_svg, equivalent_lines = equivalent_lines)
-	except:
-		traceback.print_exc(file=sys.stdout)
+	equivalent_lines = request.args.get("equivalent_lines")
+	if equivalent_lines is not None:
+		equivalent_lines = equivalent_lines.strip().split("\n")
+		equivalent_lines = map( lambda line: line.strip(),  equivalent_lines)
+		fsm_code = get_fsm_code( equivalent_lines)
+		fsm_svg = get_svg(fsm_code)
+	else:
+		fsm_svg = ""
+	return render_template('index.html', svg = fsm_svg, equivalent_lines = equivalent_lines)
 
 if __name__ == '__main__':
 	app.run(debug=True)
