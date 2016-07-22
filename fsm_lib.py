175edb1768b82c437b096dcf78f2214299fb4764
Harsh Trivedi <harshjtrivedi94@gmail.com>
2016-07-22 16:41:10 +0530

    update squeeze method so that epsilon addition goes little more smoothly

--- fsm_lib.py
+++ fsm_lib.py
@@ -158,7 +158,6 @@ class Fsm:
 
 
 	def sqeeze(self):
-
 		all_merges_made = set()
 		merges_made = 100
 		while merges_made > 0:
@@ -171,9 +170,9 @@ class Fsm:
 						nodes = map( lambda key: node_x.nexts[key], keys)
 						for node_a, node_b in itertools.combinations( nodes, 2):
 							if node_a.id != node_b.id:
-
+								intersection = [x for x in [node_a.id, node_b.id] if x in [self.start.id, self.end.id]]
 								if not all_merges_made.__contains__( "-".join([str(node_a.id), str(node_b.id)])):
-									if not self.is_connected(node_a, node_b):
+									if not self.is_connected(node_a, node_b) and len(intersection) == 0:
 										if node_b in self.all_nodes:
 											self.merge_fsm_nodes(node_a, node_b)
 											all_merges_made.add("-".join([str(node_a.id), str(node_b.id)]))
@@ -216,8 +215,9 @@ class Fsm:
 						nodes = map( lambda key: node_x.previouses[key], keys)
 						for node_a, node_b in itertools.combinations( nodes, 2):
 							if node_a.id != node_b.id:
+								intersection = [x for x in [node_a.id, node_b.id] if x in [self.start.id, self.end.id]]
 								if not all_merges_made.__contains__( "-".join([str(node_a.id), str(node_b.id)])):
-									if not self.is_connected(node_a, node_b):
+									if not self.is_connected(node_a, node_b) and len(intersection) == 0:
 										if node_b in self.all_nodes:
 											self.merge_fsm_nodes(node_a, node_b)
 											all_merges_made.add("-".join([str(node_a.id), str(node_b.id)]))
